9222c21cf56d6c65e5db449d840c411d86edbaab
Patrick Kille <pr.kille@gmail.com>
2020-04-24 16:32:50 -0700

    Updates

---
+++ precache-manifest.50508cbe01ddee8bf7ef2599bd25397e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02084721a6b597cf5ac0306ea91ae7d",
+    "url": "/TapRoom/index.html"
+  },
+  {
+    "revision": "3b35142199f8d3173875",
+    "url": "/TapRoom/static/css/main.1c56fc17.chunk.css"
+  },
+  {
+    "revision": "c1602b1b5e9cd00f32fa",
+    "url": "/TapRoom/static/js/2.d5f5b2d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TapRoom/static/js/2.d5f5b2d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b35142199f8d3173875",
+    "url": "/TapRoom/static/js/main.be8582dc.chunk.js"
+  },
+  {
+    "revision": "fed23b4c35781f101869",
+    "url": "/TapRoom/static/js/runtime-main.e6260de6.js"
+  }
+]);
